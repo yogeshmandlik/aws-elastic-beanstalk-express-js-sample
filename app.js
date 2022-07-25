c208de4b91c0810c67eb2430479399477dec0d6a
yogeshmandlik <ymandlik08@gmail.com>
2022-07-25 11:06:46 +0530

    Change in app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first step towards Devops Project on AWS, thanks for your patience!'));
+app.get('/', (req, res) => res.send('Shree Swami Samarth!Swami Om, Swamiom....Om Swami$$$$$'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
